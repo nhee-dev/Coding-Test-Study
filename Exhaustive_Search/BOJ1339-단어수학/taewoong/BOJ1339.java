34cb1b7232d6f5bcf7e0bbd21890de7cc570ced5
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-02-09 22:05:33 +0900

    [code] BOJ1339 / 커밋방법 / 완전탐색

---
+++ Exhaustive_Search/BOJ1339-단어수학/taewoong/BOJ1339.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BOJ1339 {
+	
+	static char[] arr;
+    static int size = 0;
+
+    public static void cal(int n) {
+        for (int i = 0; i < size; i++) {
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("1339.txt"));
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = Integer.parseInt(sc.nextLine());
+        String[] s = new String[N];
+        HashSet<Character> set = new HashSet<>();
+        arr = new char[10];
+
+        for (int i = 0; i < N; i++) {
+            s[i] = sc.nextLine();
+        }
+
+//        System.out.println('A' - 'A');
+        for (String word : s) {
+            for (int i = 0; i < s.length; i++) {
+                set.add(word.charAt(i));
+            }
+        }
+
+        size = 0;
+        for (char c : set) {
+            arr[size++] = c;
+        }
+
+        cal(9);
+    }
+}
