525c0a8e504c57629e15febbf5daa335cf1d59b1
yeonbb98 <yeonbb98@naver.com>
2022-02-25 08:57:13 +0900

    [code] hyeyeon / BOJ12845 / 모두의마블 / 그리디

---
+++ Greedy/BOJ12845-모두의마블/hyeyeon/BOJ_12485.java
@@ -0,0 +1,41 @@
+package A0223;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_12485 {
+
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		int index=0;
+		int max=0;
+		
+		for(int i=0;i<N;i++) {
+			if(max<arr[i]) {
+				max=arr[i];
+				index=i;
+			}
+		}
+		int sum=0;
+		for(int i=0;i<N;i++) {
+			if(i!=index) {
+				sum+=arr[i];
+			}
+		}
+		
+		int answer = max*(N-1)+sum;
+		System.out.println(answer);
+		
+	}
+}
