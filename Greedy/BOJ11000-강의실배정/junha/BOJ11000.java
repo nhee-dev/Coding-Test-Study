f275abf30f0a28811a1b5314692b79c7caaac2c2
LeeJunha <junha03011@gmail.com>
2022-02-25 11:00:52 +0900

    [code] junha / BOJ11000 / 강의실 배정 / greedy

--- Greedy/BOJ11000-강의실배정/junha/BOJ11000.java
+++ Greedy/BOJ11000-강의실배정/junha/BOJ11000.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
+ 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
